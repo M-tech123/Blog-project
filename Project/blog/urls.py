14c8f17f4daba083bb657af2c7ec6e2fc08dd20c
Muhammad Salah <ms505351@gmail.com>
2025-07-05 02:59:02 +0300

    Adding pagination and calss based

--- Project/blog/urls.py
+++ Project/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 app_name = 'blog' 
 
 urlpatterns = [
-    path('', views.posts,name='post'),  
+    path('', views.PostListView.as_view(),name='post'),  
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),  
 
 ]
--- Project/blog/views.py
+++ Project/blog/views.py
@@ -1,13 +1,29 @@
 from django.shortcuts import render, HttpResponse
 from django.http import Http404
 from .models import Post
-
+from django.core.paginator import Paginator
+from django.core.paginator import EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
 
 # Create your views here.
-def posts(request):
-    posts = Post.objects.all()
-    return render(request, 'blog/posts.html', {'posts': posts})
+# def posts(request):
+#     post_list = Post.objects.all()
+#     paginator = Paginator(post_list, 2) 
+#     page_number = request.GET.get('page', 1)
 
+#     try:
+#         posts = paginator.get_page(page_number)
+#     except PageNotAnInteger:
+#         posts = paginator.get_page(1)
+#     except EmptyPage:
+#         posts = paginator.get_page(paginator.num_pages)
+#     return render(request, 'blog/post/posts.html', {'posts': posts})
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post/posts.html'
+    context_object_name = 'posts'
+    paginate_by = 2
 
 def post_detail(request, year, month, day, post):
     try:
@@ -20,4 +36,4 @@ def post_detail(request, year, month, day, post):
         )
     except Post.DoesNotExist:
         raise Http404("Post does not exist")
-    return render(request, 'blog/post_detail.html', {'post': post})
+    return render(request, 'blog/post/post_detail.html', {'post': post})
